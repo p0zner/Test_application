36fd1e63478035955ccfe3401c73214a30cdea75
Eugene Poznyak <poznyak_jenia@mail.ru>
2025-11-17 22:48:25 +0300

    Revert "refactor: move productContent to new component from ProductDetails"
    This reverts commit d89d0bec07f61748656eda8b6f47e10890dfb394.

--- src/pages/ProductDetails/ProductDetails.tsx
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -1,12 +1,13 @@
 import {useParams} from 'react-router-dom';
 import {useAppSelector} from "../../store/hooks/useAppSelector.ts";
+import {convertPrice} from "../../utils/price.utils.ts";
 import Loader from "../../components/Loader/Loader.tsx";
 import {selectProductById} from "../../store/selectors/selectProductById.ts";
 
 import classes from './productDetails.module.scss';
 import NotFoundPage from "../NotFoundPage/NotFoundPage.tsx";
 import ActionBar from "../../components/ActionBar/ActionBar.tsx";
-import ProductContent from "./ProductContent/ProductContent.tsx";
+import ProductImage from "./ProductImage/ProductImage.tsx";
 
 const ProductDetailPage = () => {
     const {productId} = useParams<{ productId: string }>();
@@ -24,8 +25,17 @@ const ProductDetailPage = () => {
 
     return (
         <div className={classes.pageContainer}>
-            <ActionBar product={product} />
-            <ProductContent product={product} />
+            <ActionBar product={product}/>
+            {product && (
+                <div className={classes.contentGrid}>
+                    <ProductImage product={product}/>
+                    <div className={classes.infoContainer}>
+                        <h1 className={classes.title}>{product.title}</h1>
+                        <p className={classes.description}>{product.description}</p>
+                        <div className={classes.price}>{convertPrice(product.price)} ₽</div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
