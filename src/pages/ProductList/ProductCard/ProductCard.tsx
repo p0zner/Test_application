794ca0431668b84df71bcdc74d1c19511de575cf
Eugene Poznyak <poznyak_jenia@mail.ru>
2025-11-17 12:43:29 +0300

    move handleDelete function from productList to productCard

--- src/pages/ProductList/ProductCard/ProductCard.tsx
+++ src/pages/ProductList/ProductCard/ProductCard.tsx
@@ -2,41 +2,56 @@ import type {Product} from "../../../types/product.ts";
 import classes from './productCard.module.scss'
 import {Link} from 'react-router-dom';
 import DeleteButton from "../../../components/DeleteButton/DeleteButton.tsx";
-import {memo} from "react";
+import {memo, useCallback} from "react";
 import {convertPrice} from "../../../utils/price.utils.ts";
+import {deleteProduct} from "../../../store/thunks/productsThunk.ts";
+import {useAppDispatch} from "../../../store/hooks/useAppDispatch.ts";
+import {toast} from "react-toastify";
 
 interface ProductCardProps {
     product: Product;
-    onDelete: (id: string) => Promise<void>;
 }
 
-const ProductCard = memo(({product, onDelete}: ProductCardProps) => (
-        <div className={classes.productCard}>
-            <div className={classes.deleteButtonTopRight}>
-                <DeleteButton
-                    id={product.id}
-                    onDelete={onDelete}
-                    displayMode="overlay"
-                />
-            </div>
-            <img src={product.image} alt={product.title} />
-            <div className={classes.productContent}>
-                <div className={classes.mainCardInfo}>
-                    <div className={classes.title}>{product.title}</div>
-                    <div className={classes.price}>{convertPrice(product.price)} ₽</div>
+const ProductCard = memo(({product}: ProductCardProps) => {
+    const dispatch = useAppDispatch();
+
+    const onDelete = useCallback(async (id: string) => {
+        try {
+            await dispatch(deleteProduct(id)).unwrap();
+            toast.success("Товар удален");
+        } catch (error) {
+            toast.error('Не удалось удалить товар')
+        }
+    }, [dispatch])
+
+    return (
+            <div className={classes.productCard}>
+                <div className={classes.deleteButtonTopRight}>
+                    <DeleteButton
+                        id={product.id}
+                        onDelete={onDelete}
+                        displayMode="overlay"
+                    />
                 </div>
-                <p>{product.description}</p>
+                <img src={product.image} alt={product.title} />
+                <div className={classes.productContent}>
+                    <div className={classes.mainCardInfo}>
+                        <div className={classes.title}>{product.title}</div>
+                        <div className={classes.price}>{convertPrice(product.price)} ₽</div>
+                    </div>
+                    <p>{product.description}</p>
+                </div>
+                <div className={classes.productHandlers}>
+                    <Link
+                        to={`/products/${product.id}`}
+                        className={`${classes.button} ${classes.showInfoButton}`}
+                    >
+                        Перейти
+                    </Link>
+                </div>
             </div>
-            <div className={classes.productHandlers}>
-                <Link
-                    to={`/products/${product.id}`}
-                    className={`${classes.button} ${classes.showInfoButton}`}
-                >
-                    Перейти
-                </Link>
-            </div>
-        </div>
-    )
+        )
+    }
 )
 
 
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,25 +1,12 @@
 import ProductCard from "./ProductCard/ProductCard.tsx";
+import Loader from "../../components/Loader/Loader.tsx";
+import {useAppSelector} from "../../store/hooks/useAppSelector.ts";
 
 import classes from './productList.module.scss';
-import Loader from "../../components/Loader/Loader.tsx";
-import {useAppDispatch} from "../../store/hooks/useAppDispatch.ts";
-import {useAppSelector} from "../../store/hooks/useAppSelector.ts";
-import {toast} from "react-toastify";
-import {useCallback} from "react";
-import {deleteProduct} from "../../store/thunks/productsThunk.ts";
+
 
 const ProductList = () => {
-    const dispatch = useAppDispatch();
     const {items: products, status, error} = useAppSelector(state => state.products);
-
-    const handleDelete = useCallback(async (id: string) => {
-        try {
-            await dispatch(deleteProduct(id)).unwrap();
-            toast.success("Товар удален");
-        } catch (error) {
-            toast.error('Не удалось удалить товар')
-        }
-    }, [dispatch])
 
     if (status === 'loading') {
         return <Loader />;
@@ -37,7 +24,6 @@ const ProductList = () => {
                 <ProductCard
                     key={product.id}
                     product={product}
-                    onDelete={handleDelete}
                 />
             ))}
         </div>
